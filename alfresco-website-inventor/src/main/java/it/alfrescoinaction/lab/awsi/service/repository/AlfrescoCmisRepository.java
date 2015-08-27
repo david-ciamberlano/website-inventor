926910a467c11a863f8f08dc61784eb452c303d9
David <david.ciamberlano@gmail.com>
2015-08-27 19:30:00 +0200

    Refactoring and re-engineering

--- alfresco-website-inventor/src/main/java/it/alfrescoinaction/lab/awsi/service/repository/AlfrescoCmisRepository.java
+++ alfresco-website-inventor/src/main/java/it/alfrescoinaction/lab/awsi/service/repository/AlfrescoCmisRepository.java
@@ -1,14 +1,12 @@
 package it.alfrescoinaction.lab.awsi.service.repository;
 
-import it.alfrescoinaction.lab.awsi.domain.WebPage;
 import it.alfrescoinaction.lab.awsi.service.AlfrescoRemoteConnection;
 import it.alfrescoinaction.lab.awsi.service.RemoteConnection;
 import org.apache.chemistry.opencmis.client.api.*;
-import org.apache.chemistry.opencmis.commons.exceptions.CmisObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.NoSuchElementException;
 
 
 @Repository
@@ -19,21 +17,8 @@ public class AlfrescoCmisRepository implements CmisRepository {
 
     private String alfrescoHomePath;
 
-    /**
-     * Build the domain object representing a webpage
-     * @param id the id of the page to build
-     * @return the WebPage object
-     * @throws CmisObjectNotFoundException
-     */
-    public WebPage buildWebPage (String id) throws CmisObjectNotFoundException{
-
-        WebPage wp = new WebPage();
-
+    public Folder getFolderById(String id) throws NoSuchElementException {
         Session session = connection.getSession();
-
-        if (session != null) {
-            throw new CmisObjectNotFoundException();
-        }
 
         if (id.equals("home")) {
             id = session.getObjectByPath(alfrescoHomePath).getId();
@@ -43,31 +28,36 @@ public class AlfrescoCmisRepository implements CmisRepository {
 
         // procceed only if the node is a folder
         if (obj.getType().getId().equals("cmis:folder")){
-
             Folder folder = (Folder)obj;
-            wp.setId(id);
-
-            // retrieve the parent id
-            wp.setParentId(folder.getParentId());
-            wp.setTitle(obj.getName());
-
-            OperationContext oc = session.createOperationContext();
-            oc.setRenditionFilterString("*");
-            ItemIterable<CmisObject> children = folder.getChildren(oc);
-
-            for (CmisObject cmiso : children) {
-                // path relative to the homepage
-                if (cmiso.getType().getId().equals("cmis:folder")) {
-                    wp.addLinks(cmiso.getName(), cmiso.getId());
-                }
-                else if (cmiso.getType().getId().equals("cmis:document")) {
-                    Document doc = (Document)cmiso;
-                    wp.addContent(doc);
-                }
-            }
+            return folder;
         }
+        else {
+            throw new NoSuchElementException("Folder not found: "  + id);
+        }
+    }
 
-        return wp;
+    public Document getDocumentById(String id) throws NoSuchElementException {
+        Session session = connection.getSession();
+
+        CmisObject obj = session.getObject(id);
+
+        if (obj.getType().getId().equals("cmis:document")){
+            Document doc = (Document)obj;
+            return doc;
+        }
+        else {
+            throw new NoSuchElementException("Document not found: "  + id);
+        }
+    }
+
+    public ItemIterable<CmisObject> getChildren (Folder folder) {
+
+        Session session = connection.getSession();
+        OperationContext oc = session.createOperationContext();
+        oc.setRenditionFilterString("*");
+        ItemIterable<CmisObject> children = folder.getChildren(oc);
+
+        return children;
     }
 
 
