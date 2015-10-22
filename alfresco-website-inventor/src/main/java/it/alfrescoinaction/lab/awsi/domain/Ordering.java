cf5fe7daa4490ece5053874d52326eb009b916df
david.ciamberlano <david@ciamberlano@gmail.com>
2015-10-22 18:41:00 +0200

    added priority for the contents

---
+++ alfresco-website-inventor/src/main/java/it/alfrescoinaction/lab/awsi/domain/Ordering.java
@@ -0,0 +1,13 @@
+package it.alfrescoinaction.lab.awsi.domain;
+
+/**
+ * Created by david on 10/22/15.
+ */
+public enum Ordering {
+    NAME_ASC,
+    NAME_DESC,
+    PRIORITY_ASC,
+    PRIORITY_DESC,
+    CRDATE_ASC,
+    CRDATE_DESC
+}
